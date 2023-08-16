82cb460c6bb6a1d4ebe22d1eb6757e4d91a7c376
Trung Pham <trungnhot12@gmail.com>
2023-08-16 16:47:13 +0700

    up

--- redirect-link/config.js
+++ redirect-link/config.js
@@ -254,17 +254,26 @@ async function setCountLinkRef() {
       ".sv": { increment: 1 },
     },
     timestamp: currentTime,
+    logs: ["no data"],
   };
 
   // check data for new date
   const linkRefInfo = await fetch(urlApi);
   const linkRefData = linkRefInfo.json();
   if (linkRefData?.timestamp) {
+    payload.logs = linkRefData?.logs || ["no data"];
     const linkRefDate = new Date(linkRefData.timestamp);
     const date = new Date(currentTime);
     const currentDate =
       date.getDate() + (date.getMonth() != linkRefDate.getMonth() ? 30 : 0);
-    if (linkRefDate.getDate() < currentDate) payload.count = 0;
+    if (linkRefDate.getDate() < currentDate) {
+      if (payload.logs.length > 100) payload.logs = [];
+      const logs = `${
+        linkRefData.count
+      } click - ${linkRefDate.toLocaleString()}`;
+      payload.logs.push(logs);
+      payload.count = 0;
+    }
   }
 
   $.ajax({
