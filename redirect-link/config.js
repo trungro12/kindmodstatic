75ce04db651fe387a69433892326d80e3697db52
Trung Pham <trungnhot12@gmail.com>
2023-08-23 14:15:47 +0700

    up

--- redirect-link/config.js
+++ redirect-link/config.js
@@ -160,18 +160,19 @@ function showAds() {
     .append(`<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2242202070298795"
     crossorigin="anonymous"></script>`);
   $(".adsense").empty().append(`
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2242202070298795"
-    crossorigin="anonymous"></script>
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-2242202070298795"
      data-ad-slot="1551808879"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
     `);
+
+  setTimeout(() => {
+    $(".adsense").append(`<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>`);
+  }, 1000);
 }
 $(async () => {
   if (queryUrl) {
